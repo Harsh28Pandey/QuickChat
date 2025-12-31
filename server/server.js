c2dcab97f4217a8a12131793ef7527ce4e2f68f2
Harsh Pandey <harsh28.knp@gmail.com>
2025-12-31 18:38:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,15 +40,7 @@ io.on("connection", (socket) => {
 
 // middleware setup
 app.use(express.json({ limit: "4mb" }))
-// Allow only your frontend
-app.use(cors({
-    origin: "https://quick-chat-delta-three.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true // if you use cookies or auth headers
-}));
-
-app.use(express.json());
-// app.use(cors())
+app.use(cors())
 
 // routes setup
 app.use("/api/status", (req, res) => res.send("Server is Live..."))
